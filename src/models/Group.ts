936723265b7ff0ac8ecdf1d61421b120ef8be172
Kamran-Ingo <47444293+Kamran-Ingo@users.noreply.github.com>
2020-12-20 19:09:03 +0100

    groupe

--- src/models/Group.ts
+++ src/models/Group.ts
@@ -1,13 +1,17 @@
+import User from './User';
+
 export default class Group {
     private idGroup : number;
     private nom : string;
+    private createur: User;
     // private asset: Asset;
 
 
 
-    constructor(idGroup: number, name: string, idCreateur: number  ){
+    constructor(idGroup: number, name: string, idCreateur: number, creator: User  ){
         this.idGroup = idGroup;
         this.nom = name;
+        this.createur = creator;
     }
 
     get id(): number {
@@ -16,6 +20,10 @@ export default class Group {
 
     get name(): string {
         return this.nom;
+    };
+
+    get creator(): User {
+        return this.createur;
     };
 
 };
